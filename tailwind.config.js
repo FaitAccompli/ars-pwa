a57524ad9ef2422f5caec2262c9161616d1439da
FaitAccompli <raymart.montesclaros@gmail.com>
2023-06-18 13:08:54 +0800

    (chore): added tailwindcss, postcss and autprefixer

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+	content: [],
+	theme: {
+		extend: {}
+	},
+	plugins: [],
+	purge: ['./index.html', './src/**/*.{svelte,js,ts}'], // for unused CSS
+	variants: {
+		extend: {}
+	}
+};
